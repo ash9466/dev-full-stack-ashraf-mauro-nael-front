8b76c091ea461ed6352d532c38a824e1b8e43015
Nael Amirat <amiratnael@gmail.com>
2025-01-05 19:55:02 +0100

    set up react project with tailwind + sign up form done

---
+++ my-project/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import { SignUpForm } from './SignUpForm';
+
+function App() {
+  return (
+      <div>
+        <SignUpForm></SignUpForm>
+      </div>
+  );
+}
+
+export default App;
